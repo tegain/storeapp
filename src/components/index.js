09afde7078b22ee71945f24bf314447f7d535797
Thomas <thomas.egain@gmail.com>
2017-11-01 19:50:23 +0100

    Add static assets and edit data json file

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,6 @@
 import StoreHeader from '@/components/header/Header'
 import HeaderCart from '@/components/header/HeaderCart'
 import HeaderNavBtn from '@/components/header/HeaderNavBtn'
-import HomeBackground from '@/components/home/Background'
 import HomeSlider from '@/components/home/Slider'
 import ProductCard from '@/components/product/Card'
 
@@ -14,7 +13,6 @@ const Components = [
   { name: 'header-nav-btn', template: HeaderNavBtn },
 
   // Home
-  { name: 'home-background', template: HomeBackground },
   { name: 'home-slider', template: HomeSlider },
   { name: 'product-card', template: ProductCard }
 ]
--- src/components/product/Card.vue
+++ src/components/product/Card.vue
@@ -1,12 +1,34 @@
 <template>
   <div class="Store-product__card">
-    <strong>Slide n°{{ product.index }}</strong><br>
-    {{ product.name }}
+    <img :src="product.picture.small" alt="" :width="product.picturesSizes.small" class="Store-product__picture">
+    <h2 class="Store-product__name">{{ product.name | capitalize }}</h2>
   </div>
 </template>
 
 <script>
   export default {
-    props: ['product']
+    props: ['product'],
+
+    filters: {
+      capitalize (value) {
+        if (!value) return ''
+        value = value.toString()
+        return value.charAt(0).toUpperCase() + value.slice(1)
+      }
+    }
   }
 </script>
+
+<style lang="scss">
+  .Store-product {
+
+    &__card {
+      padding: 0 1.5rem 1.5rem;
+    }
+
+    &__picture {
+      margin-top: -30%;
+      max-width: 75%;
+    }
+  }
+</style>

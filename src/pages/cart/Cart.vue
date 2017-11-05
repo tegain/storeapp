2fcd9c80f95531a00f79500f48e7f841861772e7
Thomas <thomas.egain@gmail.com>
2017-11-05 16:49:26 +0100

    WIP - Cart page

--- src/pages/cart/Cart.vue
+++ src/pages/cart/Cart.vue
@@ -1,5 +1,25 @@
 <template>
   <div class="Store-content" data-template="cart">
     Cart page content
+    <div class="item" v-for="(product, key) in cart" :key="product.index">
+      {{ product.name }}
+    </div>
   </div>
 </template>
+
+<script>
+  // import { mapGetters } from 'vuex'
+
+  export default {
+    data () {
+      return {
+        cart: this.$store.state.cart
+      }
+    }
+    /* computed: {
+      ...mapGetters([
+        'cartInfos'
+      ])
+    }, */
+  }
+</script>
